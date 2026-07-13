41f4929c81aaa89019092d8e6d3853dbde118c1a
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-07-13 16:28:07 +0900

    use my JS Function from Haskell

--- docs/haskell/try_import/some_module.js
+++ docs/haskell/try_import/some_module.js
@@ -1,3 +1,8 @@
-export default function logging() {
+export default function logging(foo) {
 	console.log("Hello, module world!");
+	console.log(foo);
+}
+
+global.GlobalLog = function(foo) {
+	console.log(foo);
 }
