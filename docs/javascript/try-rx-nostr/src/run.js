f194828fb132561b45623ca5b5a0dc38763781f6
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-07-14 10:09:45 +0900

    try-rx-nostr: move import to run.js

--- docs/javascript/try-rx-nostr/src/run.js
+++ docs/javascript/try-rx-nostr/src/run.js
@@ -1,2 +1,8 @@
-import('./import.js');
+import { createRxNostr, createRxForwardReq } from "https://esm.sh/";
+import { verifier } from "https://esm.sh/";
+
+globalThis.createRxNostr = createRxNostr;
+globalThis.createRxForwardReq = createRxForwardReq;
+globalThis.verifier = verifier;
+
 import('./index.js');
