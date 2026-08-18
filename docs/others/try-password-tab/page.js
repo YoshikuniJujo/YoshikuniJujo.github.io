972d7e1f197123127b9467f377fd048be8dc4964
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-08-18 14:00:58 +0900

    try-password-tab: remove old

--- docs/others/try-password-tab/page.js
+++ docs/others/try-password-tab/page.js
@@ -1,19 +1,4 @@
 const result = document.getElementById("result");
-const result2 = document.getElementById("result2");
-
-document.querySelector("#open-input").addEventListener("click", async () => {
-
-	result.textContent = "入力待ち...";
-
-	try {
-		const value = await window.test.openInputTab();
-		result.textContent = `結果: ${value}`;
-	} catch (e) {
-		result.textContent = `エラー: ${e}`;
-	}
-
-});
-
 
 document.querySelector("#open-input2").addEventListener("click", async () => {
 
