aebd6c9e6be5010e3ae352ca7c070b48b9189055
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-08-19 11:20:51 +0900

    try-password-tab

--- docs/others/try-password-tab/page.js
+++ docs/others/try-password-tab/page.js
@@ -5,7 +5,7 @@ document.querySelector("#open-input").addEventListener("click", async () => {
 	result.textContent = "入力待ち...";
 
 	try {
-		const value = await window.test.openInputTab();
+		const value = await window.tryPasswordTab.openInputTab();
 		result.textContent = `結果: ${value}`;
 	} catch (e) {
 		result.textContent = `エラー: ${e}`;
@@ -20,7 +20,7 @@ async function openInputTabByShortcut(event) {
 
 		result.textContent = "入力待ち...";
 		try {
-			const value = await window.test.openInputTab();
+			const value = await window.tryPasswordTab.openInputTab();
 			result.textContent = `結果: ${value}`;
 		} catch (e) {
 			result.textContent = `エラー: ${e}`;
