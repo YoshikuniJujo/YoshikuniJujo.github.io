dddee9d44ec7b9db35e206485c2163391db1254f
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-08-22 18:40:13 +0900

    try-password-tab

--- docs/others/try-password-tab/page.js
+++ docs/others/try-password-tab/page.js
@@ -1,31 +1,23 @@
 const result = document.getElementById("result");
 
 document.querySelector("#open-input").addEventListener("click", async () => {
+	queryInput(123) });
 
-	result.textContent = "入力待ち...";
+document.addEventListener("keydown", (ev) => openInputTabByShortcut(123, ev));
 
-	try {
-		const value = await window.tryPasswordTab.queryInput("123");
-		result.textContent = `結果: ${value}`;
-	} catch (e) {
-		result.textContent = `エラー: ${e}`;
-	}
-
-});
-
-async function openInputTabByShortcut(event) {
-	if (event.ctrlKey && event.shiftKey && event.key.toLowerCase() === "f"
-	) {
-		event.preventDefault();
-
-		result.textContent = "入力待ち...";
-		try {
-			const value = await window.tryPasswordTab.queryInput("123");
-			result.textContent = `結果: ${value}`;
-		} catch (e) {
-			result.textContent = `エラー: ${e}`;
-		}
-	}
+async function
+openInputTabByShortcut(a, event)
+{
+	if (event.ctrlKey && event.shiftKey &&
+		event.key.toLowerCase() === "f") {
+		event.preventDefault(); queryInput(123); }
 }
 
-document.addEventListener("keydown", openInputTabByShortcut);
+async function
+queryInput(a)
+{
+	result.textContent = "入力待ち...";
+	try {	const value = await window.tryPasswordTab.queryInput(a);
+		result.textContent = `結果: ${value}`;
+	} catch (e) { result.textContent = `エラー: ${e}`; }
+}
