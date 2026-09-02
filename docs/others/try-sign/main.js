63e598edf9828cb07cd20d75e80c7c3e25f32600
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-09-02 14:03:21 +0900

    try-sign

--- docs/others/try-sign/main.js
+++ docs/others/try-sign/main.js
@@ -1,15 +1,22 @@
-console.log("main.js loaded");
-console.log(window.nostr);
-
 const event = {
 	created_at: 1750000000,
 	kind: 1,
 	tags: [],
 	content: "Hello, Nostr!" };
 
-const pubKey = await window.nostr.getPublicKey();
+const nostr = await waitForNostr();
+
+const pubKey = await nostr.getPublicKey();
 document.querySelector("#public-key").textContent = pubKey;
 
-const signedEvent = await window.nostr.signEvent(event);
+const signedEvent = await nostr.signEvent(event);
 document.querySelector("#signed-event").textContent =
 	JSON.stringify(signedEvent, null, 2);
+
+async function
+waitForNostr()
+{
+	while (!window.nostr)
+		await new Promise(resolve => setTimeout(resolve, 500));
+	return window.nostr;
+}
