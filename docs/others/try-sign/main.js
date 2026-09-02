3ff294d533c46d629fb42f4d57060f85b4426670
Yoshikuni Jujo <yoshikuni.jujo@gmail.com>
2026-09-02 13:42:49 +0900

    try-sign

---
+++ docs/others/try-sign/main.js
@@ -0,0 +1,12 @@
+const event = {
+	created_at: 1750000000,
+	kind: 1,
+	tags: [],
+	content: "Hello, Nostr!" };
+
+const pubKey = await window.nostr.getPublicKey();
+document.querySelector("#public-key").textContent = pubKey;
+
+const signedEvent = await window.nostr.signEvent(event);
+document.querySelector("#signed-event").textContent =
+	JSON.stringify(signedEvent, null, 2);
